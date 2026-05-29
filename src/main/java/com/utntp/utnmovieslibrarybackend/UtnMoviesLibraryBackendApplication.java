bd4436cd4a61b7c081c0441f4f599c94b87b8d8d
santimat <santimaturo1@gmail.com>
2026-05-29 14:23:38 -0300

    no sé que estoy haciendo ya

---
+++ src/main/java/com/utntp/utnmovieslibrarybackend/UtnMoviesLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.utntp.utnmovieslibrarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnMoviesLibraryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtnMoviesLibraryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utntp/utnmovieslibrarybackend/UtnMoviesLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utntp.utnmovieslibrarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnMoviesLibraryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
