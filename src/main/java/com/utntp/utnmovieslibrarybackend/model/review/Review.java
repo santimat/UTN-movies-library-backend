7bc548e1a170aea8786e645841d3cfbf9b2ade6e
santimat <santimaturo1@gmail.com>
2026-05-29 19:43:14 -0300

    add constructors, getters and setters

--- src/main/java/com/utntp/utnmovieslibrarybackend/model/review/Review.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/model/review/Review.java
@@ -17,4 +17,55 @@ public class Review {
     private Long userId;
     @Column(nullable = false)
     private Long movieId;
+
+    public Review() {
+    }
+
+    public Review(Long id, Integer rating, String comment, Long userId, Long movieId) {
+        this.id = id;
+        this.rating = rating;
+        this.comment = comment;
+        this.userId = userId;
+        this.movieId = movieId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
 }
