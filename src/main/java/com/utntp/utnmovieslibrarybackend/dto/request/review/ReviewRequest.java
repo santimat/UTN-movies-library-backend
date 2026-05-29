979def6f9214ea144412af788ef94a0058add8c0
guido perroud <guidoperroud@gmail.com>
2026-05-29 20:49:45 -0300

    finish review dto

---
+++ src/main/java/com/utntp/utnmovieslibrarybackend/dto/request/review/ReviewRequest.java
@@ -0,0 +1,50 @@
+package com.utntp.utnmovieslibrarybackend.dto.request.review;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class ReviewRequest {
+
+    @NotNull
+    private Integer rating;
+    @NotBlank
+    private String comment;
+    @NotNull
+    private Long movieId;
+
+
+    public ReviewRequest() {
+    }
+
+
+    public ReviewRequest(Integer rating, String comment, Long movieId) {
+        this.rating = rating;
+        this.comment = comment;
+        this.movieId = movieId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Long movieId) {
+        this.movieId = movieId;
+    }
+}
