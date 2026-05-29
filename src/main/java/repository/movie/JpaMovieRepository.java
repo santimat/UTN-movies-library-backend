6ff97e35f984a34109a42e7e538c53ed5404efd0
santimat <santimaturo1@gmail.com>
2026-05-29 19:32:04 -0300

    no sé

--- src/main/java/repository/movie/JpaMovieRepository.java
+++
@@ -1,9 +0,0 @@
-package repository.movie;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaMovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
-}
--- src/main/java/repository/review/JpaReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package repository.review;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaReviewRepository extends JpaRepository<Review, Long>, JpaSpecificationExecutor<Review> {
-}
