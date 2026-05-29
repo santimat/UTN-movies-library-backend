596b73dc8a6b5bff3f1b7bc9bebca1a41ff81766
santimat <santimaturo1@gmail.com>
2026-05-29 20:12:15 -0300

    create movie request and movie response

---
+++ src/main/java/com/utntp/utnmovieslibrarybackend/dto/response/movie/MovieResponse.java
@@ -0,0 +1,92 @@
+package com.utntp.utnmovieslibrarybackend.dto.response.movie;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class MovieResponse {
+    @NotNull
+    private Long id;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String director;
+    @NotBlank
+    private String genre;
+    @NotBlank
+    private String synopsis;
+    @NotNull
+    private Integer year;
+    @Nullable
+    private String posterUrl;
+
+    public MovieResponse() {
+    }
+
+    public MovieResponse(Long id, String title, String director, String genre, String synopsis, Integer year, @Nullable String posterUrl) {
+        this.id = id;
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+        this.synopsis = synopsis;
+        this.year = year;
+        this.posterUrl = posterUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    @Nullable
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(@Nullable String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+}
