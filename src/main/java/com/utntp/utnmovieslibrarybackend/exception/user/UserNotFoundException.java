fb3411ca1af5017145eb635a12f9d24dcdd3f82b
santimat <santimaturo1@gmail.com>
2026-05-29 20:00:18 -0300

    add user not found exception for user finder ervice

---
+++ src/main/java/com/utntp/utnmovieslibrarybackend/exception/user/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.utntp.utnmovieslibrarybackend.exception.user;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(Long id) {
+        super("User with id " + id + " not found");
+    }
+}
