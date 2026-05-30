6e1628083d6c39b1c165dca3f5a6eb2809db99b3
guido perroud <guidoperroud@gmail.com>
2026-05-30 17:40:30 -0300

    finish review controller and add movie post controller

---
+++ src/main/java/com/utntp/utnmovieslibrarybackend/controller/review/ReviewPutController.java
@@ -0,0 +1,31 @@
+package com.utntp.utnmovieslibrarybackend.controller.review;
+
+import com.utntp.utnmovieslibrarybackend.dto.request.review.ReviewRequest;
+import com.utntp.utnmovieslibrarybackend.dto.response.review.ReviewResponse;
+import com.utntp.utnmovieslibrarybackend.mapper.review.ReviewMapper;
+import com.utntp.utnmovieslibrarybackend.model.review.Review;
+import com.utntp.utnmovieslibrarybackend.service.review.ReviewUpdaterService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/reviews")
+public class ReviewPutController {
+    private final ReviewUpdaterService reviewUpdaterService;
+    private final ReviewMapper reviewMapper;
+
+
+    public ReviewPutController(ReviewUpdaterService reviewUpdaterService, ReviewMapper reviewMapper) {
+        this.reviewUpdaterService = reviewUpdaterService;
+        this.reviewMapper = reviewMapper;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ReviewResponse> put(@PathVariable Long id,
+                                              @Valid @RequestBody ReviewRequest reviewRequest){
+        Review review = reviewUpdaterService.update(id, reviewRequest);
+        ReviewResponse reviewResponse = reviewMapper.toResponse(review);
+        return ResponseEntity.ok(reviewResponse);
+    }
+}
