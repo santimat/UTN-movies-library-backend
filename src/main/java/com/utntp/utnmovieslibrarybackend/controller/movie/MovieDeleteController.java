aa038cb46c3cdc4cd9dafc029872f9bd75f31541
guido perroud <guidoperroud@gmail.com>
2026-05-30 20:19:23 -0300

    finish movie controllers

---
+++ src/main/java/com/utntp/utnmovieslibrarybackend/controller/movie/MovieDeleteController.java
@@ -0,0 +1,25 @@
+package com.utntp.utnmovieslibrarybackend.controller.movie;
+
+import com.utntp.utnmovieslibrarybackend.service.movie.MovieDeleterService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/movies")
+public class MovieDeleteController {
+    private final MovieDeleterService movieDeleterService;
+
+
+    public MovieDeleteController(MovieDeleterService movieDeleterService) {
+        this.movieDeleterService = movieDeleterService;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        movieDeleterService.deleter(id);
+        return ResponseEntity.noContent().build();
+    }
+}
