bf26749bcd800174c68de82d72405d91c6c18371
santimat <santimaturo1@gmail.com>
2026-06-01 20:03:21 -0300

    add findByEmail for user

--- src/main/java/com/utntp/utnmovieslibrarybackend/repository/user/JpaUserRepository.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/repository/user/JpaUserRepository.java
@@ -3,8 +3,12 @@ package com.utntp.utnmovieslibrarybackend.repository.user;
 import com.utntp.utnmovieslibrarybackend.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaUserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    @Query("SELECT users from users WHERE email = :userEmail")
+    User findByEmail(@Param(value = "userEmail") String userEmail);
 }
