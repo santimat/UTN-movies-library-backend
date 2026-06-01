d09207c1a58adb4512753b113f001d84577b1b59
Guido <guidoperroud@gmail.com>
2026-06-01 20:12:25 -0300

    login service done

--- src/main/java/com/utntp/utnmovieslibrarybackend/service/user/UserCreatorService.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/service/user/UserCreatorService.java
@@ -3,6 +3,7 @@ package com.utntp.utnmovieslibrarybackend.service.user;
 import com.utntp.utnmovieslibrarybackend.dto.request.user.UserRequest;
 import com.utntp.utnmovieslibrarybackend.mapper.user.UserMapper;
 import com.utntp.utnmovieslibrarybackend.model.user.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.utntp.utnmovieslibrarybackend.repository.user.JpaUserRepository;
 
@@ -10,14 +11,17 @@ import com.utntp.utnmovieslibrarybackend.repository.user.JpaUserRepository;
 public class UserCreatorService {
     private final JpaUserRepository jpaUserRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserCreatorService(JpaUserRepository jpaUserRepository){
+    public UserCreatorService(JpaUserRepository jpaUserRepository, UserMapper userMapper, PasswordEncoder passwordEncoder){
         this.jpaUserRepository = jpaUserRepository;
-        this.userMapper = new UserMapper();
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public User create(UserRequest userRequest){
-        User user = userMapper.toEntity(userRequest);
+    public User create(UserRequest request){
+        User user = userMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
         return jpaUserRepository.save(user);
     }
 }
