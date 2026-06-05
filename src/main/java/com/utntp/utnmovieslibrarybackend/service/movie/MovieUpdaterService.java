6222073ec9369fc0692a6e141ef9e08af736aece
santimat <santimaturo1@gmail.com>
2026-06-05 12:43:14 -0300

    apply same logic that in creator movie service

--- src/main/java/com/utntp/utnmovieslibrarybackend/service/movie/MovieUpdaterService.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/service/movie/MovieUpdaterService.java
@@ -1,25 +1,39 @@
 package com.utntp.utnmovieslibrarybackend.service.movie;
 
 import com.utntp.utnmovieslibrarybackend.dto.request.movie.MovieRequest;
+import com.utntp.utnmovieslibrarybackend.model.genre.Genre;
 import com.utntp.utnmovieslibrarybackend.model.movie.Movie;
+import com.utntp.utnmovieslibrarybackend.repository.genre.JpaGenreRepository;
 import com.utntp.utnmovieslibrarybackend.repository.movie.JpaMovieRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MovieUpdaterService {
     private final JpaMovieRepository jpaMovieRepository;
     private final MovieFinderService movieFinderService;
+    private final JpaGenreRepository jpaGenreRepository;
 
 
-    public MovieUpdaterService(JpaMovieRepository jpaMovieRepository, MovieFinderService movieFinderService) {
+
+    public MovieUpdaterService(JpaMovieRepository jpaMovieRepository, MovieFinderService movieFinderService, JpaGenreRepository jpaGenreRepository) {
         this.jpaMovieRepository = jpaMovieRepository;
         this.movieFinderService = movieFinderService;
+        this.jpaGenreRepository = jpaGenreRepository;
     }
 
     public Movie update(Long id, MovieRequest movieRequest){
+        Optional<Genre> existsGenre = jpaGenreRepository.findByName(movieRequest.getGenre());
         Movie toUpdate = movieFinderService.find(id);
+        if(existsGenre.isPresent()) toUpdate.setGenre(existsGenre.get());
+        else{
+            Genre newGenre = new Genre();
+            newGenre.setName(movieRequest.getGenre());
+            Genre savedGenre = jpaGenreRepository.save(newGenre);
+            toUpdate.setGenre(savedGenre);
+        }
         toUpdate.setDirector(movieRequest.getDirector());
-        toUpdate.setGenre(movieRequest.getGenre());
         toUpdate.setSynopsis(movieRequest.getSynopsis());
         toUpdate.setTitle(movieRequest.getTitle());
         toUpdate.setYear(movieRequest.getYear());
