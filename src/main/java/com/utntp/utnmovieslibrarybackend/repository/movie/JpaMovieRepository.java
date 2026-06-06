393c2efda07e708c5d3417449c3b1dc530c28b9d
santimat <santimaturo1@gmail.com>
2026-06-06 13:12:44 -0300

    add method to get all movies by genre

--- src/main/java/com/utntp/utnmovieslibrarybackend/repository/movie/JpaMovieRepository.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/repository/movie/JpaMovieRepository.java
@@ -1,10 +1,16 @@
 package com.utntp.utnmovieslibrarybackend.repository.movie;
 
 import com.utntp.utnmovieslibrarybackend.model.movie.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaMovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
+    @Query("SELECT m FROM Movie m WHERE LOWER(m.genre.name) = LOWER(:genreName)")
+    Page<Movie> findAllByGenre(@Param(value = "genreName") String genre, Pageable pageable);
 }
