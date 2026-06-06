e5d5f8ae37341c62fa0f3ab5dc2994fb85ccbe11
santimat <santimaturo1@gmail.com>
2026-06-06 13:13:48 -0300

    change movies get controller to allow sort and search by genre by search param

--- src/main/java/com/utntp/utnmovieslibrarybackend/controller/movie/MoviesGetController.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/controller/movie/MoviesGetController.java
@@ -3,10 +3,12 @@ package com.utntp.utnmovieslibrarybackend.controller.movie;
 import com.utntp.utnmovieslibrarybackend.dto.response.movie.MovieResponse;
 import com.utntp.utnmovieslibrarybackend.mapper.movie.MovieMapper;
 import com.utntp.utnmovieslibrarybackend.model.movie.Movie;
+import com.utntp.utnmovieslibrarybackend.service.movie.MoviesFinderByGenreService;
 import com.utntp.utnmovieslibrarybackend.service.movie.MoviesSearcherService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,19 +19,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/movies")
 public class MoviesGetController {
     private final MoviesSearcherService moviesSearcherService;
+    private final MoviesFinderByGenreService moviesFinderByGenreService;
     private final MovieMapper movieMapper;
 
 
-    public MoviesGetController(MoviesSearcherService moviesSearcherService) {
+    public MoviesGetController(MoviesSearcherService moviesSearcherService, MoviesFinderByGenreService moviesFinderByGenreService) {
         this.moviesSearcherService = moviesSearcherService;
+        this.moviesFinderByGenreService = moviesFinderByGenreService;
         this.movieMapper = new MovieMapper();
     }
 
     @GetMapping
     public ResponseEntity<Page<MovieResponse>> getMovies(@RequestParam(defaultValue = "0") int page,
-                                                      @RequestParam(defaultValue = "5") int size){
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Movie> movies = moviesSearcherService.findAll(pageable);
+                                                      @RequestParam(defaultValue = "5") int size,
+                                                         @RequestParam(defaultValue = "title") String sortBy,
+                                                         @RequestParam(defaultValue = "ASC") String sortOrder,
+                                                         @RequestParam(required = false) String genre){
+
+        Sort.Direction sortDirection = Sort.Direction.fromString(sortOrder);
+        Sort sortConfig = Sort.by(sortDirection, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sortConfig);
+        Page<Movie> movies = (genre == null || genre.isEmpty()) ? moviesSearcherService.findAll(pageable) :
+                moviesFinderByGenreService.findAllByGenre(genre, pageable);
+
         return ResponseEntity.ok(
                 movies.map(
                         movieMapper::toResponse
