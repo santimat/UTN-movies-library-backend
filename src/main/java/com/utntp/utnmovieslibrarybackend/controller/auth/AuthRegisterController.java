e07c07ba0f0bd01a894c15856ed377716e8bb031
santimat <santimaturo1@gmail.com>
2026-06-09 11:02:00 -0300

    refactor: remove unused UserExistingValidatorService from AuthRegisterController

--- src/main/java/com/utntp/utnmovieslibrarybackend/controller/auth/AuthRegisterController.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/controller/auth/AuthRegisterController.java
@@ -5,7 +5,6 @@ import com.utntp.utnmovieslibrarybackend.dto.response.user.UserResponse;
 import com.utntp.utnmovieslibrarybackend.mapper.user.UserMapper;
 import com.utntp.utnmovieslibrarybackend.model.user.User;
 import com.utntp.utnmovieslibrarybackend.service.auth.AuthRegisterService;
-import com.utntp.utnmovieslibrarybackend.service.user.UserExistingValidatorService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,22 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth/register")
 public class AuthRegisterController {
     private final AuthRegisterService authRegisterService;
-    private final UserExistingValidatorService userExistingValidatorService;
     private final UserMapper userMapper;
 
 
-    public AuthRegisterController(AuthRegisterService authRegisterService,
-                                  UserExistingValidatorService userExistingValidatorService) {
+    public AuthRegisterController(AuthRegisterService authRegisterService) {
         this.authRegisterService = authRegisterService;
-        this.userExistingValidatorService = userExistingValidatorService;
         this.userMapper = new UserMapper();
     }
 
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@Valid @RequestBody AuthRegisterRequest userRequest){
-        if(userExistingValidatorService.existsByEmail(userRequest.getEmail())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
         User user = authRegisterService.register(userRequest);
         UserResponse userResponse = userMapper.toResponse(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
