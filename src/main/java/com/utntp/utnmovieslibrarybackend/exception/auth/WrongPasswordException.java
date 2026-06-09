88c8ffa9ebbc562e2d0419d75111368b0216b471
santimat <santimaturo1@gmail.com>
2026-06-08 21:13:06 -0300

    change response status for WrongPasswordException to UNAUTHORIZED

--- src/main/java/com/utntp/utnmovieslibrarybackend/exception/auth/WrongPasswordException.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/exception/auth/WrongPasswordException.java
@@ -3,7 +3,7 @@ package com.utntp.utnmovieslibrarybackend.exception.auth;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class WrongPasswordException extends RuntimeException {
     public WrongPasswordException() {
         super("Wrong password");
