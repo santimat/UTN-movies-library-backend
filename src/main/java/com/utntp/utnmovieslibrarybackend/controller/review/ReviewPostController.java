1e1a26a10ba9eb7fc4ab9f39c9f301e42660ab91
santimat <santimaturo1@gmail.com>
2026-06-09 11:00:43 -0300

    feat: enhance ReviewCreatorService and ReviewPostController for improved review creation

--- src/main/java/com/utntp/utnmovieslibrarybackend/controller/review/ReviewPostController.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/controller/review/ReviewPostController.java
@@ -5,10 +5,12 @@ import com.utntp.utnmovieslibrarybackend.dto.response.review.ReviewResponse;
 import com.utntp.utnmovieslibrarybackend.mapper.review.ReviewMapper;
 import com.utntp.utnmovieslibrarybackend.model.review.Review;
 import com.utntp.utnmovieslibrarybackend.security.JwtService;
+import com.utntp.utnmovieslibrarybackend.security.UserPrincipal;
 import com.utntp.utnmovieslibrarybackend.service.review.ReviewCreatorService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,10 +28,8 @@ public class ReviewPostController {
 
     @PostMapping
     public ResponseEntity<ReviewResponse> createReview(@Valid @RequestBody ReviewRequest reviewRequest,
-                                                       @CookieValue(value = "token") String token){
-
-        Long userId = jwtService.getUserId(token);
-        Review review = reviewCreatorService.create(reviewRequest, userId);
+                                                       @AuthenticationPrincipal UserPrincipal userPrincipal){
+        Review review = reviewCreatorService.create(reviewRequest, userPrincipal.getId());
         ReviewResponse reviewResponse = reviewMapper.toResponse(review);
         return ResponseEntity.status(HttpStatus.CREATED).body(reviewResponse);
     }
--- src/main/java/com/utntp/utnmovieslibrarybackend/service/review/ReviewCreatorService.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/service/review/ReviewCreatorService.java
@@ -1,36 +1,42 @@
 package com.utntp.utnmovieslibrarybackend.service.review;
 
 import com.utntp.utnmovieslibrarybackend.dto.request.review.ReviewRequest;
+import com.utntp.utnmovieslibrarybackend.exception.ResourceNotFoundException;
 import com.utntp.utnmovieslibrarybackend.mapper.review.ReviewMapper;
 import com.utntp.utnmovieslibrarybackend.model.movie.Movie;
 import com.utntp.utnmovieslibrarybackend.model.review.Review;
 import com.utntp.utnmovieslibrarybackend.model.user.User;
+import com.utntp.utnmovieslibrarybackend.repository.movie.JpaMovieRepository;
 import com.utntp.utnmovieslibrarybackend.repository.review.JpaReviewRepository;
 import com.utntp.utnmovieslibrarybackend.service.movie.MovieFinderService;
 import com.utntp.utnmovieslibrarybackend.service.user.UserFinderService;
+import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewCreatorService {
     private final JpaReviewRepository jpaReviewRepository;
+    private final JpaMovieRepository jpaMovieRepository;
     private final ReviewMapper reviewMapper;
-    private final MovieFinderService movieFinderService;
-    private final UserFinderService userFinderService;
+    private final EntityManager entityManager;
 
 
-    public ReviewCreatorService(JpaReviewRepository jpaReviewRepository,  MovieFinderService movieFinderService,
-                                UserFinderService userFinderService) {
+    public ReviewCreatorService(JpaReviewRepository jpaReviewRepository, EntityManager entityManager,
+                                JpaMovieRepository jpaMovieRepository) {
         this.jpaReviewRepository = jpaReviewRepository;
-        this.movieFinderService = movieFinderService;
-        this.userFinderService = userFinderService;
+        this.jpaMovieRepository = jpaMovieRepository;
+        this.entityManager = entityManager;
         this.reviewMapper = new ReviewMapper();
     }
 
     public Review create(ReviewRequest reviewRequest, Long userId){
-        Movie movie = movieFinderService.findById(reviewRequest.getMovieId());
-        User user = userFinderService.findById(userId);
-        Review review = reviewMapper.toEntity(reviewRequest, movie);
-        review.setUser(user);
+        if(!jpaMovieRepository.existsById(reviewRequest.getMovieId()))
+            throw new ResourceNotFoundException("Movie with id " + reviewRequest.getMovieId() + " not found");
+
+        Movie movieProxy = entityManager.getReference(Movie.class, reviewRequest.getMovieId());
+        User userProxy = entityManager.getReference(User.class, userId);
+        Review review = reviewMapper.toEntity(reviewRequest, movieProxy);
+        review.setUser(userProxy);
         return jpaReviewRepository.save(review);
     }
 }
