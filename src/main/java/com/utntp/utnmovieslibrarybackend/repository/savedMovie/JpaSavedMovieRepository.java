9a70d13d949e6eb62138067a5f05a0c177150276
santimat <santimaturo1@gmail.com>
2026-06-09 01:32:00 -0300

    feat: add JpaSavedMovieRepository for managing saved movies with user-specific queries

---
+++ src/main/java/com/utntp/utnmovieslibrarybackend/repository/savedMovie/JpaSavedMovieRepository.java
@@ -0,0 +1,15 @@
+package com.utntp.utnmovieslibrarybackend.repository.savedMovie;
+
+import com.utntp.utnmovieslibrarybackend.model.savedMovie.SavedMovie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JpaSavedMovieRepository extends JpaRepository<SavedMovie, Long>, JpaSpecificationExecutor<SavedMovie> {
+    Page<SavedMovie> findByUserId(Long userId, Pageable pageable);
+    boolean existsByUserIdAndMovieId(Long userId, Long movieId);
+    void deleteByUserIdAndMovieId(Long userId, Long movieId);
+}
