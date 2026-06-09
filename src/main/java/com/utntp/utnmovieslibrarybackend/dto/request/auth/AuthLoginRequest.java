4e913f3eb9e138308d2d64b24480fc7c96fca739
santimat <santimaturo1@gmail.com>
2026-06-09 13:50:49 -0300

    refactor: add validation constraints for email, password, and name in AuthLoginRequest and AuthRegisterRequest

--- src/main/java/com/utntp/utnmovieslibrarybackend/dto/request/auth/AuthLoginRequest.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/dto/request/auth/AuthLoginRequest.java
@@ -1,25 +1,44 @@
 package com.utntp.utnmovieslibrarybackend.dto.request.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+
 public class AuthLoginRequest {
+    @Email(message = "Email should be valid")
     private String email;
+    @Min(value = 6, message = "Password should be at least 6 characters")
     private String password;
     private boolean remember;
 
-    public AuthLoginRequest(){}
+    public AuthLoginRequest() {
+    }
 
-    public AuthLoginRequest(String email, String password, boolean remember){
+    public AuthLoginRequest(String email, String password, boolean remember) {
         this.email = email;
         this.password = password;
         this.remember = remember;
     }
 
-    public String getEmail(){return email;}
-    public String getPassword(){return password;}
-    public void setEmail(String email){this.email = email;}
-    public void setPassword(String password){this.password = password;}
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public boolean isRemember() {
         return remember;
     }
+
     public void setRemember(boolean remember) {
         this.remember = remember;
     }
--- src/main/java/com/utntp/utnmovieslibrarybackend/dto/request/auth/AuthRegisterRequest.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/dto/request/auth/AuthRegisterRequest.java
@@ -1,14 +1,21 @@
 package com.utntp.utnmovieslibrarybackend.dto.request.auth;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 public class AuthRegisterRequest {
     @NotBlank
+    @Min(value = 3, message = "Name should be at least 2 characters")
+    @Max(value = 50, message = "Name should be at most 50 characters")
     private String name;
     @NotBlank
+    @Email(message = "Email should be valid")
     private String email;
     @NotBlank
+    @Min(value = 6, message = "Password should be at least 6 characters")
     private String password;
 
     public AuthRegisterRequest() {
