248b77d9c0160dcb3c28933fb2000563976e7723
santimat <santimaturo1@gmail.com>
2026-06-09 11:02:30 -0300

    refactor: simplify AuthVerifyUserController by removing unused services and updating endpoint

--- src/main/java/com/utntp/utnmovieslibrarybackend/controller/auth/AuthVerifyUserController.java
+++ src/main/java/com/utntp/utnmovieslibrarybackend/controller/auth/AuthVerifyUserController.java
@@ -1,36 +1,30 @@
 package com.utntp.utnmovieslibrarybackend.controller.auth;
 
 import com.utntp.utnmovieslibrarybackend.dto.response.user.UserResponse;
-import com.utntp.utnmovieslibrarybackend.service.jwt.JwtService;
-import com.utntp.utnmovieslibrarybackend.service.user.UserExistingValidatorService;
-import org.springframework.http.HttpStatus;
+import com.utntp.utnmovieslibrarybackend.security.UserPrincipal;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/auth/user")
+@RequestMapping("/api/auth/me")
 public class AuthVerifyUserController {
-    private final UserExistingValidatorService userExistingValidatorService;
-    private final JwtService jwtService;
 
 
-    public AuthVerifyUserController(UserExistingValidatorService userExistingValidatorService, JwtService jwtService) {
-        this.userExistingValidatorService = userExistingValidatorService;
-        this.jwtService = jwtService;
+    public AuthVerifyUserController() {
     }
 
     @GetMapping
-    public ResponseEntity<UserResponse> verifyAuth(@CookieValue(name = "token") String token) {
-        if (token == null || token.isEmpty() || !jwtService.isTokenValid(token)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        String email = jwtService.getEmail(token);
-        if (!userExistingValidatorService.existsByEmail(email)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        return ResponseEntity.ok(jwtService.getUserData(token));
+    public ResponseEntity<UserResponse> verifyAuth(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        UserResponse userResponse = new UserResponse(
+                userPrincipal.getId(),
+                userPrincipal.getUsername(),
+                userPrincipal.getEmail(),
+                userPrincipal.getRole().name()
+        );
+
+        return ResponseEntity.ok(userResponse);
     }
 }
